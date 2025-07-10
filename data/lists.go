44c61cf18f0999b13b548a13803a38324d864470
Marco Orlandi <Gianluca1996@gmx.de>
2025-07-10 21:12:12 +0200

    func deepdive

--- data/lists.go
+++ data/lists.go
@@ -8,7 +8,13 @@ func main() {
 	fmt.Println(prices)
 
 	productNames[2] = "A Carpet"
-	fmt.Println(productNames)
 
+	fmt.Println(productNames)
 	fmt.Println(prices[2])
+
+	featuredPrices := prices[1:]
+	highlightedPrices := featuredPrices[:1]
+	fmt.Println(highlightedPrices)
+	fmt.Println(len(featuredPrices), cap(featuredPrices))
+
 }
